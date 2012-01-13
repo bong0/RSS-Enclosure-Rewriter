56c3e7df1521745b3095b3a86639f0763fa570d7
bongo <bongo@bird.localdomain>
2012-01-13 17:45:28 +0100

    moved README

--- enc_rewrite.py
+++ enc_rewrite.py
@@ -17,9 +17,8 @@ from urllib.request import urlopen
 from urllib.parse import urlparse, quote
 
 ##### CONSTANTS #######
-podcastDir = str("CHANGE")
-feedDir = podcastDir+"/feeds/" # CREATE!
-externalUrl = "http://example.com/podcasts"
+podcastDir = str("/var/www/virtual/bongo/html/podcasts/")
+externalUrl = "http://bongo.scorpius.uberspace.de/podcasts/"
 keepPodcasts = 3 # keep n last podcasts of each feed
 #######################
 categories = set()
@@ -111,7 +110,7 @@ with open("/home/bongo/.podget/serverlist", "r") as servers: #read and extract r
             if(output and output=="verb"):
               print("modded to "+enclosure.attrib["url"])
       
-    with open(feedDir+podcastName+".rss", "wb") as feed:
+    with open("/home/bongo/html/podcasts/feeds/"+podcastName+".rss", "wb") as feed:
       feed.write(etree.tostring(tree, encoding="UTF-8"))
 
 #chmod podcasts
